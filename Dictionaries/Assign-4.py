5479f4faec7099130b9b8aa4fc20dddec7c37750
Rohini <rohinibiradar05@gmail.com>
2025-06-12 19:31:55 +0530

    initial commit

---
+++ Dictionaries/Assign-4.py
@@ -0,0 +1,13 @@
+student = {
+    'personal_info': {
+        'name': 'Rohini',
+        'age': 21,
+        'id': '2125'
+    },
+    'grades': {
+        'math': 88,
+        'science': 92,
+        'english': 85
+    }
+}
+print("Math Grade:", student['grades']['math'])
